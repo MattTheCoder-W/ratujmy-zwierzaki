f494510f1f7c924c9a8620e5b0211741e70f5aec
MattTheCoder-W <mateusz.wasa@protonmail.com>
2022-08-29 13:05:05 +0200

    Added place info

--- check-perc.py
+++ check-perc.py
@@ -10,8 +10,9 @@ def check_perc():
     driver = webdriver.Firefox(options=opt)
     driver.get("https://whatwevalue.telekom.com/pl-PL/projects/2B7EbLBBdvXrBQsGbhIasl")
     sleep(2)
+    place = driver.find_elements(By.XPATH, '/html/body/div/div/div[1]/main/div/div[9]/div[2]/div[1]/div[1]/p[2]')[0]
     txt = driver.find_elements(By.XPATH, '/html/body/div/div/div[1]/main/div/div[9]/div[2]/div[1]/div[2]/p')[0]
-    print(txt.get_attribute("innerText"))
+    print("Miejsce", place.get_attribute("innerText"), txt.get_attribute("innerText"))
 
 
 if __name__ == "__main__":
