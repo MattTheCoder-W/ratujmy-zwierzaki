e933ffc66418abb79415424e414aab847dd9cacb
MattTheCoder-W <mateusz.wasa@protonmail.com>
2022-08-29 11:46:53 +0200

    Added max-threads option

--- ratuj.py
+++ ratuj.py
@@ -11,6 +11,7 @@ from fake_useragent import UserAgent
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.alert import Alert
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
@@ -23,21 +24,26 @@ SAVED = 0
 START = datetime.now()
 
 class Ratownik:
-    def __init__(self, obj_id: int, timeout: int = 1, max_timeout: int = 60) -> None:
+    def __init__(self, obj_id: int, timeout: int = 1, max_timeout: int = 15) -> None:
+        self.running = False
         self.ID = obj_id
         self.laps = 0
+        self.page_url = "https://whatwevalue.telekom.com/pl-PL/projects/2B7EbLBBdvXrBQsGbhIasl"
+        self.cookies_accept_xpath = "/html/body/div/div/div[1]/main/div[1]/div[2]/div/div[2]/div[2]/button[1]"
+        self.like_xpath = "/html/body/div/div/div[1]/main/div/div[9]/div[2]/div[2]/button"
+        self.TIMEOUT = timeout
+        self.MAX_TIMEOUT = max_timeout
+        self.driver = self.wait = None
+        self.create_browser()
 
+    def create_browser(self) -> None:
         options = Options()
         options.headless = True
         options.add_argument(f'user-agent={UserAgent().random}')
 
         self.driver = webdriver.Firefox(options=options)
-        self.wait = WebDriverWait(self.driver, max_timeout)
-        
-        self.page_url = "https://whatwevalue.telekom.com/pl-PL/projects/2B7EbLBBdvXrBQsGbhIasl"
-        self.cookies_accept_xpath = "/html/body/div/div/div[1]/main/div[1]/div[2]/div/div[2]/div[2]/button[1]"
-        self.like_xpath = "/html/body/div/div/div[1]/main/div/div[9]/div[2]/div[2]/button"
-        self.TIMEOUT = timeout
+        self.wait = WebDriverWait(self.driver, self.MAX_TIMEOUT)
+        print(f"[browser-{self.ID}] Instance recreated!")
 
     def get_dialog(self, driv) -> list:
         return self.driver.find_elements(By.CSS_SELECTOR, 'vbox.dialogOverlay:nth-child(1) > vbox:nth-child(1) > browser:nth-child(2)')
@@ -62,29 +68,42 @@ class Ratownik:
         return self.driver.find_elements(By.XPATH, self.cookies_accept_xpath)
 
     def save_animal(self) -> None:
-        try:
-            global SAVED
-            global START
-            prefix = Fore.MAGENTA + Style.BRIGHT + f"[browser-{Fore.RED}{self.ID}{Fore.MAGENTA}, lap={Fore.RED}{self.laps}{Fore.MAGENTA}]" + Style.RESET_ALL
-            self.driver.get(self.page_url)
-            sleep(1.5)
-            self.wait.until(self.get_accept_cookies)
-            self.get_accept_cookies(None)[0].click()
-            self.driver.execute_script('document.querySelector(".boost-us-button").scrollIntoView();')
-            sleep(1.5)
-            self.driver.find_elements(By.XPATH, self.like_xpath)[0].click()
-            self.laps += 1
+        while self.running:
+            sleep(1)
+            print("Waiting for finish")
+        while True:
+            try:
+                self.running = True
+                global SAVED
+                global START
+                prefix = Fore.MAGENTA + Style.BRIGHT + f"[browser-{Fore.RED}{self.ID}{Fore.MAGENTA}, lap={Fore.RED}{self.laps}{Fore.MAGENTA}]" + Style.RESET_ALL
+                while True:
+                    self.driver.get(self.page_url)
+                    sleep(1.5)
+                    try:
+                        self.wait.until(self.get_accept_cookies)
+                        break
+                    except TimeoutException:
+                        pass
+                self.get_accept_cookies(None)[0].click()
+                self.driver.execute_script('document.querySelector(".boost-us-button").scrollIntoView();')
+                sleep(1.5)
+                self.driver.find_elements(By.XPATH, self.like_xpath)[0].click()
+                self.laps += 1
 
-            print(f"{prefix} -- time {str(datetime.now() - START).split('.')[0]} -- [CPM:{round(SAVED/(datetime.now() - START).total_seconds(),10)*60}] [{SAVED}] " + Fore.GREEN + "Animal saved!" + Style.RESET_ALL)
-            SAVED += 1
-            self.clear_cookies()
-            c_timeout = randint(self.TIMEOUT, self.TIMEOUT+3)
-            print(f"{prefix} " + Fore.YELLOW + "INFO:" + Style.RESET_ALL + f" Cookies cleared, waiting {c_timeout} seconds before restart")
-            sleep(c_timeout)
-        except Exception as e:
-            with open("log.log", "a") as f:
-                print(Fore.RED + Style.BRIGHT + "Got Error! Skipping..." + Style.RESET_ALL)
-                f.write(str(type(e)) + str(e) + "\n")
+                print(f"{prefix} -- time {str(datetime.now() - START).split('.')[0]} -- [CPM:{round(SAVED/(datetime.now() - START).total_seconds(),10)*60}] [{SAVED}] " + Fore.GREEN + "Animal saved!" + Style.RESET_ALL)
+                SAVED += 1
+                self.clear_cookies()
+                self.running = False
+                print(f"{prefix} " + Fore.YELLOW + "INFO:" + Style.RESET_ALL + f" Cookies cleared, waiting {self.TIMEOUT} seconds before restart")
+                sleep(self.TIMEOUT)
+            except Exception as e:
+                with open("log.log", "a") as f:
+                    print(Fore.RED + Style.BRIGHT + "Got Error! Skipping..." + Style.RESET_ALL)
+                    f.write(str(type(e)) + str(e) + "\n")
+                self.create_browser()
+                continue
+            break
 
     def close(self) -> None:
         self.driver.quit()
@@ -96,6 +115,7 @@ if __name__ == "__main__":
     parser.add_argument("laps", type=int, help="Liczba powtórzeń kliknięcia (na każdej z przeglądarek)")
     parser.add_argument("--timeout", type=int, help="Czas do odczekania po każdym like (domyślnie 1 sek)")
     parser.add_argument("--threads", type=int, help="Liczba wątków (przeglądarek) na raz (uwaga na zużycie RAMu!)")
+    parser.add_argument("--max-timeout", type=int, help="Czas po jakim przeglądarka uznawana jest za martwą")
     parser.add_argument("--not-headless", action="store_true", help="Nie chowa przeglądrek z widoku")
     parser.add_argument("--quiet", "-q", action="store_true", help="Program nie będzie nic wypisywał w konsoli")
     args = vars(parser.parse_args())
@@ -105,6 +125,7 @@ if __name__ == "__main__":
     HEADLESS: bool = not args['not_headless']
     THREADS: int = int(args['threads']) if args['threads'] is not None else 1
     TIMEOUT: int  = int(args['timeout']) if args['timeout'] is not None else 1
+    MAX_TIMEOUT: int = int(args['max_timeout']) if args['max_timeout'] is not None else 15
 
     if THREADS <= 0:
         print(Fore.RED + Style.BRIGHT + "[ERROR] [threads] Proszę o dodatnią wartość" + Style.RESET_ALL)
@@ -113,10 +134,10 @@ if __name__ == "__main__":
     ratownicy = []
     for i in range(THREADS):
         print(f"[{i+1}/{THREADS}] Started")
-        ratownicy.append(Ratownik(i, TIMEOUT))
+        ratownicy.append(Ratownik(i, TIMEOUT, MAX_TIMEOUT))
 
     START = datetime.now()
-    last_printed = -1
+    last_printed = 0
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=THREADS) as executor:
         futures = []
