e9fe0a4c114557dc4fa1a614b58945a2fd2af3ad
MattTheCoder-W <mateusz.wasa@protonmail.com>
2022-08-29 12:41:38 +0200

    Added percent checker

---
+++ check-perc.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from time import sleep
+
+
+def check_perc():
+    opt = Options()
+    opt.headless = True
+    driver = webdriver.Firefox(options=opt)
+    driver.get("https://whatwevalue.telekom.com/pl-PL/projects/2B7EbLBBdvXrBQsGbhIasl")
+    sleep(2)
+    txt = driver.find_elements(By.XPATH, '/html/body/div/div/div[1]/main/div/div[9]/div[2]/div[1]/div[2]/p')[0]
+    print(txt.get_attribute("innerText"))
+
+
+if __name__ == "__main__":
+    check_perc()
